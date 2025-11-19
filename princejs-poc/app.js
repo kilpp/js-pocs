5041004863a561df1999a83378daa9c5154a5803
Guilherme Kilpp <guikilpp@gmail.com>
2025-11-19 17:15:31 -0300

    Added SSE endpoint

--- princejs-poc/app.js
+++ princejs-poc/app.js
@@ -4,4 +4,16 @@ const app = prince();
 
 app.get("/", () => ({ message: "Hello!" }));
 
+app.ws("/chat", {
+  open: (ws) => {
+    ws.send("Welcome to the chat!");
+  },
+  message: (ws, msg) => {
+    ws.send(`Echo: ${msg}`);
+  },
+  close: () => {
+    console.log("User disconnected");
+  }
+});
+
 app.listen(3000);
